cec4b22d98f39f554e7e0c58ea7a1fe193bf41b7
jame mackson <jamemackson@icloud.com>
2022-03-08 10:08:26 -0600

    added from

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 8080;
 
 express()
   .get("/", (req, res) => {
-    res.send("Hello Control Plane!");
+    res.send("Hello from Control Plane!");
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
